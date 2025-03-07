86289980d98d0c60cca91f879b48c36e19e9f754
tusharts <72263744+tusharts@users.noreply.github.com>
2025-03-07 00:13:52 +0000

    php

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,22 @@
-  <?php
-    /**
-    * Requires the "PHP Email Form" library
-    * The "PHP Email Form" library is available only in the pro version of the template
-    * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-    * For more info and help: https://bootstrapmade.com/php-email-form/
-    */
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $to = "tushar@tusharts.me";
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
 
-    // Replace contact@example.com with your real receiving email address
-    $receiving_email_address = 'betusharsingh@gmail.com';
+    $headers = "From: $name <$email>\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-    if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-      include( $php_email_form );
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Success!";
     } else {
-      die( 'Unable to load the "PHP Email Form" Library!');
+        echo "Error sending email!";
     }
-
-    $contact = new PHP_Email_Form;
-    $contact->ajax = true;
-    
-    $contact->to = $receiving_email_address;
-    $contact->from_name = $_POST['name'];
-    $contact->from_email = $_POST['email'];
-    $contact->subject = $_POST['subject'];
-
-    // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-    /*
-    $contact->smtp = array(
-      'host' => 'example.com',
-      'username' => 'example',
-      'password' => 'pass',
-      'port' => '587'
-    );
-    */
-
-    $contact->add_message( $_POST['name'], 'From');
-    $contact->add_message( $_POST['email'], 'Email');
-    $contact->add_message( $_POST['message'], 'Message', 10);
-
-    echo $contact->send();
-  ?>
+} else {
+    http_response_code(405);
+    echo "405 Not Allowed";
+}
+?>
